b4fed7ee3e2edd2b0f33bd53a56d2c8f2fef07ac
hsirish98 <hannahsirish@gmail.com>
2023-03-13 10:03:11 -0700

    added description of variables to function header

--- R/compute_wind_power.R
+++ R/compute_wind_power.R
@@ -4,6 +4,12 @@
 ## Kinetic energy will be calculated following the forumla KE = 1/2 (moment of inertia*rotational speed)
 ## we don't have the rotational speed but for this simplification we will say the wind speed works!
 
+## @blade_length is the length of the blades of the wind turbine, in meters
+## @wind_speed is the speed of the wind that is blowing to turn the turbines
+## @mass is the total mass of the rotating blades
+## @CP is the "coefficient of performance" or efficiency of the wind turbine
+## power will be in watts and KE will be in Joules
+
 compute_wind_power <- function (blade_length, wind_speed, CP=0.45, pi= 22/7, mass=2360) {
   
   ## if blade_length <= 0 or wind_speed is <0, stops the function as it's illogical
@@ -19,7 +25,7 @@ compute_wind_power <- function (blade_length, wind_speed, CP=0.45, pi= 22/7, mas
   inertia <- ((blade_length^2)*mass) ##caculate moment of inertia
   
   
- KE <- (0.5*inertia*wind^2) ## calculate kinetic energy
+   KE <- (0.5*inertia*wind^2) ## calculate kinetic energy
   
   results <- c(KE, power) ## make a vector containing KE and power
   
